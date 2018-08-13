bbbea03b3bf247138c92d159494931a231f1a3fa
vd <vdorr.package@gmail.com>
2018-08-13 20:23:47 +0200

    step 1

--- Main.hs
+++ Main.hs
@@ -1,9 +1,58 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns -fwarn-unused-binds -fwarn-unused-imports -fno-warn-tabs #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE LambdaCase #-}
+
+#define here (__FILE__ ++ ":" ++ show (__LINE__ :: Integer) ++ " ")
+
 module Main where
 
+import Control.Concurrent
+import Control.Monad
+import Foreign.Ptr
+
 foreign import ccall "cbits_hello" cbits_hello :: Int -> IO Int
+
+foreign import ccall "rtrt_newQueue" rtrt_newQueue :: Int -> IO (Ptr ())
+foreign import ccall "rtrt_deleteQueue" rtrt_deleteQueue :: Ptr () -> IO ()
+foreign import ccall "rtrt_queueReadAvailable" rtrt_queueReadAvailable :: Ptr () -> IO Int
+foreign import ccall "rtrt_queueWriteAvailable" rtrt_queueWriteAvailable :: Ptr () -> IO Int
+foreign import ccall "rtrt_queuePush" rtrt_queuePush :: Ptr () -> Int -> IO ()
+foreign import ccall "rtrt_queuePop" rtrt_queuePop :: Ptr () -> IO Int
+
+loop x f = f x >>= flip loop f
 
 main :: IO ()
 main = do
 	cbits_hello 10 >>= print
 	putStrLn "Hello, Haskell!"
+
+	q <- rtrt_newQueue 128
+#if 0
+	rtrt_queueReadAvailable q >>= print
+	rtrt_queueWriteAvailable q >>= print
+	rtrt_queuePush q 123456789
+	rtrt_queueReadAvailable q >>= print
+	rtrt_queueWriteAvailable q >>= print
+	rtrt_queuePop q >>= print
+	rtrt_queueReadAvailable q >>= print
+	rtrt_queueWriteAvailable q >>= print
+#endif
+
+#if 1
+	forkOS $ loop 0 $ \i -> do
+--		print (here, 1)
+		threadDelay 500000
+		rtrt_queuePush q i
+		return (i + 2)
+
+	forever $ do
+		rtrt_queueReadAvailable q >>= \case
+			0 -> threadDelay 1000
+			n -> replicateM n (rtrt_queuePop q) >>= print
+--		print (here, 2)
+		return ()
+#endif
+	rtrt_deleteQueue q
+
+	print here
 
--- cbits.cpp
+++ cbits.cpp
@@ -2,7 +2,35 @@
 #include <cstdio>
 
 #include <boost/lockfree/spsc_queue.hpp>
-#include <boost/atomic.hpp>
+//#include <boost/atomic.hpp>
+
+extern "C" void* rtrt_newQueue ( int cap ) {
+	return static_cast<void*>(new boost::lockfree::spsc_queue<int>(cap));
+}
+
+extern "C" void rtrt_deleteQueue ( void* q ) {
+	delete static_cast<boost::lockfree::spsc_queue<int>*>(q);
+}
+
+extern "C" int rtrt_queueReadAvailable ( const void* q ) {
+	return static_cast<const boost::lockfree::spsc_queue<int>*>(q)->read_available();
+}
+
+extern "C" int rtrt_queueWriteAvailable ( const void* q ) {
+	return static_cast<const boost::lockfree::spsc_queue<int>*>(q)->write_available();
+}
+
+extern "C" void rtrt_queuePush ( void* q, const int v ) {
+	static_cast<boost::lockfree::spsc_queue<int>*>(q)->push(v);
+}
+
+extern "C" int rtrt_queuePop ( void* q ) {
+	int v = 0;
+	static_cast<boost::lockfree::spsc_queue<int>*>(q)->pop(&v);
+	return v;
+}
+
+#if 1
 
 boost::lockfree::spsc_queue<int, boost::lockfree::capacity<1024> > spsc_queue;
 
@@ -13,4 +41,5 @@ extern "C" int cbits_hello (int x) {
 	printf("%s:%i %i\n", __FUNCTION__, __LINE__, x);
 	return 8;
 }
+#endif
 
